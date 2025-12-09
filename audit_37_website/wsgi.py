d52edd7ee9b10fd31ad1ba216a97b3d90c08df94
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-09 14:20:21 +0500

    Update wsgi.py

--- audit_37_website/wsgi.py
+++ audit_37_website/wsgi.py
@@ -1,11 +1,1 @@
-import os
-import sys
-from pathlib import Path
-
-# Add project root to path so 'webaudit' is found
-sys.path.insert(0, str(Path(__file__).parent.parent))
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'audit_37_website.settings')
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+INSTALLED_APPS += ['webaudit']
